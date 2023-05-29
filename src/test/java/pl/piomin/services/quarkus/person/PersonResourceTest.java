37ff4dea0a5ec0b55b3661813cdeef1724dc85b1
piomin <piotr.minkowski@gmail.com>
2023-05-29 22:25:30 +0200

    adding tests + changing s2i

--- src/test/java/pl/piomin/services/quarkus/person/PersonResourceTest.java
+++ src/test/java/pl/piomin/services/quarkus/person/PersonResourceTest.java
@@ -1,19 +1,24 @@
 package pl.piomin.services.quarkus.person;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import pl.piomin.services.quarkus.person.model.Gender;
 import pl.piomin.services.quarkus.person.model.Person;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.isA;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PersonResourceTest {
 
     @Test
+    @Order(2)
     public void findAll() {
         given()
           .when().get("/persons")
@@ -23,6 +28,7 @@ public class PersonResourceTest {
     }
 
     @Test
+    @Order(2)
     public void findById() {
         Person person = given()
                 .when().get("/persons/1")
@@ -32,6 +38,39 @@ public class PersonResourceTest {
                 .body().as(Person.class);
         assertNotNull(person);
         assertEquals(1L, person.id);
+    }
+
+    @Test
+    @Order(2)
+    void getPersonByName() {
+        Person[] persons = given()
+                .pathParam("name", "TestNew")
+                .when().get("/persons/name/{name}")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Person[].class);
+        assertNotNull(persons);
+        assertTrue(persons.length > 0);
+    }
+
+    @Test
+    @Order(1)
+    void newPersonAdd() {
+        Person newPerson = new Person();
+        newPerson.age = 22;
+        newPerson.name = "TestNew";
+        newPerson.gender = Gender.FEMALE;
+        Person person = given()
+                .body(newPerson)
+                .contentType(ContentType.JSON)
+                .when().post("/persons")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().as(Person.class);
+        assertNotNull(person);
+        assertNotNull(person.id);
     }
 
 }
